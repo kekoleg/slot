d3624463939e73f031ca90468527ff3dfe7631d0
kekoleg <fotomafiozi@gmail.com>
2022-09-08 17:18:51 +0700

    MVP

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import { reelsSetup, payLines } from './constants';
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function generateReel(reelConfig) {
+  let reelSymbols = [];
+
+  for (const [symbolIndex, symbolCount] of reelConfig.entries()) {
+    reelSymbols = [
+      ...reelSymbols,
+      ...populateSymbol({
+        symbolCount,
+        elementToPush: symbolIndex,
+      }),
+    ];
+  }
+
+  return reelSymbols;
+}
+
+function populateSymbol({ symbolCount, finalElementsArray = [], elementToPush }) {
+  if (symbolCount > 0) {
+    finalElementsArray.push(elementToPush);
+    populateSymbol({ symbolCount: symbolCount - 1, finalElementsArray, elementToPush });
+  }
+  return finalElementsArray;
+}
+
+function initPlay(reelsSetup) {
+  const line = [];
+
+  for (const reelConfig of reelsSetup) {
+    const reel = generateReel(reelConfig);
+    const randomReelSymbol = reel[getRandomInt(reel.length)];
+    line.push(randomReelSymbol);
+  }
+
+  return line;
+}
+
+const playedLine = initPlay(reelsSetup);
+
+function checkLine({ playedLine, payLine }) {
+  const difference = payLine.filter((currentSymbol, index) => playedLine[index] == currentSymbol);
+
+  if (difference.length === payLine.length) {
+    return 'WIN';
+  }
+
+  return 'LOOSE';
+}
+
+function getResults(playedLine) {
+  console.log(playedLine.join(','));
+  console.log('---');
+
+  for (const currentPayLine of payLines) {
+    const playResult = checkLine({ playedLine, payLine: currentPayLine });
+
+    console.log(currentPayLine.join(','), playResult);
+  }
+}
+
+getResults(playedLine);
